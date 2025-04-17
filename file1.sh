c1c71109f84870c2748d680bc5a0b1a996328827
chewy37 <immakosgei@yahoo.com>
2025-04-16 20:09:31 -0500

    script added

--- file1.sh
+++ file1.sh
@@ -1,2 +1,8 @@
 #!/bin/bash 
-Hello world! 
+# Hello world!
+# write a script that install some packages in linux
+# write a script that install some packages in linux 
+yum install finger -y
+yum install curl -y
+yum install zip -y 
+yum install vim -y 
